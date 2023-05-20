54499f07c482f1c6c02e7c7795742c05963c33e7
fliMMM <bachkame@gmail.com>
2023-05-20 12:34:46 +0700

    add logic chat

--- chatapp/lib/screens/conversations_screen.dart
+++ chatapp/lib/screens/conversations_screen.dart
@@ -1,43 +1,24 @@
+import 'dart:ui';
+
 import 'package:chatapp/models/auth.dart';
 import 'package:chatapp/screens/chat_screen.dart';
 import 'package:chatapp/screens/Authentication/login_screen.dart';
 import 'package:chatapp/widgets/drawer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../widgets/logo.dart';
 
+final fakedata = [
+  {"sender": "hieu", "data": "Solo Aatrox khong?"},
+  {"sender": "back", "data": "thoi t so lam"},
+  {"sender": "hieu", "data": "tuong the nao"},
+  {"sender": "hieu", "data": "lan trc solo thua la do t nhuong m aatrox thoi"},
+  {"sender": "back", "data": "ghe vay sao"},
+  {"sender": "hieu", "data": "la ro, k ghe sao lai solo thang dc"},
+  {"sender": "back", "data": "m la nhat roi"},
+];
 
-final fakedata=[
-      {
-        "sender":"hieu",
-        "data":"Solo Aatrox khong?"
-      },
-      {
-        "sender":"back",
-        "data":"thoi t so lam"
-      },
-      {
-        "sender":"hieu",
-        "data":"tuong the nao"
-      },
-      {
-        "sender":"hieu",
-        "data":"lan trc solo thua la do t nhuong m aatrox thoi"
-      },
-      {
-        "sender":"back",
-        "data":"ghe vay sao"
-      },
-      {
-        "sender":"hieu",
-        "data":"la ro, k ghe sao lai solo thang dc"
-      },
-      {
-        "sender":"back",
-        "data":"m la nhat roi"
-      },
-    ];
-
-List _userFilter=fakedata;
+List _userFilter = fakedata;
 
 class ConversationScreen extends StatefulWidget {
   const ConversationScreen({super.key});
@@ -47,26 +28,33 @@ class ConversationScreen extends StatefulWidget {
 }
 
 class _ConversationScreenState extends State<ConversationScreen> {
+  var currentUserId = Auth().getCurrentUSer()?.toString();
+  final Stream<QuerySnapshot> conversationStream = FirebaseFirestore.instance
+      .collection("Conversations")
+      .where("userIds", arrayContains: Auth().getCurrentUSer()?.uid.toString())
+      .snapshots();
+
   @override
   Widget build(BuildContext context) {
-    void handleUserFilter(String text){
+    void handleUserFilter(String text) {
       // ignore: avoid_print
-      List rs=[];
-      if(text.isEmpty){
-        rs=fakedata;
-      }
-      else{
-        rs=fakedata.where((user) => user["sender"].toString().toLowerCase().contains(text.toLowerCase())).toList();
+      List rs = [];
+      if (text.isEmpty) {
+        rs = fakedata;
+      } else {
+        rs = fakedata
+            .where((user) => user["sender"]
+                .toString()
+                .toLowerCase()
+                .contains(text.toLowerCase()))
+            .toList();
       }
       setState(() {
-        _userFilter=rs;
+        _userFilter = rs;
         // ignore: avoid_print
         print(_userFilter);
       });
-
     }
-
-
 
     return Scaffold(
       appBar: AppBar(
@@ -143,53 +131,85 @@ class _ConversationScreenState extends State<ConversationScreen> {
                   ],
                 )),
             Expanded(
-                child: ListView.builder(
-              itemCount: _userFilter.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const ChatScreen(
-                                  conversationsId: "j7xp8BZ9bDM8myoSIpGl",
-                                )));
-                  },
-                  child: Row(
-                    children: [
-                      Container(
-                          height: 70,
-                          width: 70,
-                          margin: const EdgeInsets.only(
-                              left: 5, top: 5, bottom: 5, right: 15),
-                          child: const UserLogo(
-                              size: 35,
-                              imgUrl:
-                                  "https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg")),
-                      Container(
-                        width: MediaQuery.of(context).size.width - 100,
-                        height: 70,
-                        color: Colors.transparent,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Text(_userFilter[index]['sender'].toString(),
-                                style: TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.w600)),
-                            Text(_userFilter[index]['data'].toString(),
-                                style: TextStyle(
-                                    fontSize: 17, fontWeight: FontWeight.w400))
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                );
-              },
-            )),
+              child: StreamBuilder<QuerySnapshot>(
+                  stream: conversationStream,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.hasError) {
+                      return const Text('Something went wrong');
+                    }
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(
+                        child: CircularProgressIndicator(color: Colors.blue),
+                      );
+                    }
+
+                    return ListView(
+                      children: snapshot.data!.docs
+                          .map((DocumentSnapshot document) {
+                            Map<String, dynamic> data =
+                                document.data()! as Map<String, dynamic>;
+
+                            var peerId = data["userIds"][0] == currentUserId
+                                ? data["userIds"][1]
+                                : data["userIds"][0];
+                            var peer = data["user_$peerId"];
+
+                            return Card(
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => ChatScreen(
+                                                conversationsId: data["id"],
+                                              )));
+                                },
+                                child: Row(
+                                  children: [
+                                    Container(
+                                        height: 70,
+                                        width: 70,
+                                        margin: const EdgeInsets.only(
+                                            left: 5,
+                                            top: 5,
+                                            bottom: 5,
+                                            right: 15),
+                                        child: UserLogo(
+                                            size: 35,
+                                            imgUrl: peer["photoUrl"])),
+                                    Container(
+                                      width: MediaQuery.of(context).size.width -
+                                          100,
+                                      height: 70,
+                                      color: Colors.transparent,
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        children: [
+                                          Text(peer["displayName"],
+                                              style: const TextStyle(
+                                                  fontSize: 20,
+                                                  fontWeight: FontWeight.w600)),
+                                          Text(data["last_message"],
+                                              style: const TextStyle(
+                                                  fontSize: 17,
+                                                  fontWeight: FontWeight.w400))
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            );
+                          })
+                          .toList()
+                          .cast(),
+                    );
+                  }),
+            ),
           ],
         ),
       ),
